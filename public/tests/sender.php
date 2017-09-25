241345c55b66eddf51084a1f9b6410fbd91aee1c
Ben Stones <ben.stones@gatenbysanderson.com>
2017-09-25 09:37:58 +0100

    Get tests ready

--- public/tests/sender.php
+++ public/tests/sender.php
@@ -1,9 +1,13 @@
 <?php
 
-$file = new CURLFile(__DIR__ . '/example.txt');
+$files = [
+    'example' => new CURLFile(__DIR__ . '/example.html'),
+];
 
 $curl = curl_init();
 
-curl_setopt($curl, CURLOPT_URL, '192.168.33.99/receiver.php');
+curl_setopt($curl, CURLOPT_URL, 'http://192.168.33.99/tests/receiver.php');
 curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $file);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $files);
+
+curl_exec($curl);
