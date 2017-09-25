1923e0aae47aab84c195f86fbb29efcdaa0a0c20
Mubeen Inamdar <Mubeen@mubeen-Inamdar.com>
2017-09-25 16:06:01 +0100

    added alias for phpunit

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -4,3 +4,4 @@ rm -f /root/.bash_aliases && rm -f /home/ubuntu/.bash_aliases
 touch /root/.bash_aliases && touch /home/ubuntu/.bash_aliases
 
 echo "alias tinker=\"/vagrant/vendor/bin/psysh\"" | tee -a /root/.bash_aliases /home/ubuntu/.bash_aliases
+echo "alias phpunit=\"/vagrant/vendor/bin/phpunit\"" | tee -a /root/.bash_aliases /home/ubuntu/.bash_aliases
