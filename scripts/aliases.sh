329492b947e5f575a45da252fb717c1f974e07cc
Ben Stones <ben.stones@gatenbysanderson.com>
2017-09-25 11:52:25 +0100

    Add tinker support for the root user.

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -f /home/ubuntu/.bash_aliases
-touch /home/ubuntu/.bash_aliases
+rm -f /root/.bash_aliases && rm -f /home/ubuntu/.bash_aliases
+touch /root/.bash_aliases && touch /home/ubuntu/.bash_aliases
 
-echo "alias tinker=\"/vagrant/vendor/bin/psysh\"" >> /home/ubuntu/.bash_aliases
+echo "alias tinker=\"/vagrant/vendor/bin/psysh\"" | tee -a /root/.bash_aliases /home/ubuntu/.bash_aliases
